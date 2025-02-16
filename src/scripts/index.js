c4a0a2b7ecd7ad53924977a7632d0f43bbcda2a5
8luev <122008140+8luev@users.noreply.github.com>
2025-02-16 14:54:05 +1300

    Update index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         })
         .catch(error => {
-            console.error(error);
+            window.alert(error);
         });
     });
 });
